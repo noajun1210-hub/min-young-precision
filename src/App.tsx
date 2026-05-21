d64754b83166b8a8f226aea2c352b1e97931231e
noajun1210-hub <noajun1210@gmail.com>
2026-05-21 16:08:31 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import Expertise from './components/Expertise'
 import Strengths from './components/Strengths'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Admin from './pages/Admin'
 
 function App() {
+  const isAdminPage = window.location.pathname === '/admin'
+
+  if (isAdminPage) {
+    return <Admin />
+  }
+
   return (
     <div className="min-h-screen">
       <Header />
+
       <main>
         <Hero />
         <About />
@@ -19,6 +27,7 @@ function App() {
         <Strengths />
         <Contact />
       </main>
+
       <Footer />
     </div>
   )
