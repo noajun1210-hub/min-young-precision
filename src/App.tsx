37546017272e4b18d29ccc0ca5a53a0881382a9e
noajun1210-hub <noajun1210@gmail.com>
2026-05-23 08:46:44 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,60 @@
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Achievements from './components/Achievements'
-import Services from './components/Services'
-import Expertise from './components/Expertise'
-import Strengths from './components/Strengths'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import Admin from './pages/Admin'
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Achievements from './components/Achievements';
+import Services from './components/Services';
+import Expertise from './components/Expertise';
+import Strengths from './components/Strengths';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Admin from './pages/Admin';
+import {
+  AutomotiveSemiconductorPage,
+  ColletChuckPage,
+  CompanyPage,
+  MctCncPage,
+  PrecisionMachiningPage,
+} from './pages/SeoPages';
 
-function App() {
-  const isAdminPage = window.location.pathname === '/admin'
-
-  if (isAdminPage) {
-    return <Admin />
-  }
-
+function HomePage() {
   return (
-    <div className="min-h-screen">
-      <Header />
-
-      <main>
-        <Hero />
-        <About />
-        <Achievements />
-        <Services />
-        <Expertise />
-        <Strengths />
-        <Contact />
-      </main>
-
-      <Footer />
-    </div>
-  )
+    <>
+      <Hero />
+      <About />
+      <Achievements />
+      <Services />
+      <Expertise />
+      <Strengths />
+      <Contact />
+    </>
+  );
 }
 
-export default App
+function App() {
+  const normalizedPath = window.location.pathname.replace(/\/$/, '') || '/';
+
+  if (normalizedPath === '/admin') {
+    return <Admin />;
+  }
+
+  const pageMap: Record<string, JSX.Element> = {
+    '/': <HomePage />,
+    '/company': <CompanyPage />,
+    '/precision-machining': <PrecisionMachiningPage />,
+    '/mct-cnc': <MctCncPage />,
+    '/collet-chuck': <ColletChuckPage />,
+    '/automotive-semiconductor': <AutomotiveSemiconductorPage />,
+  };
+
+  const currentPage = pageMap[normalizedPath] || <HomePage />;
+
+  return (
+    <>
+      <Header />
+      {currentPage}
+      <Footer />
+    </>
+  );
+}
+
+export default App;
